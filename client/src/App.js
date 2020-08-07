e8fea6d6cc3c1c608c5333c930dbc2f9e35cc553
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-08-07 10:16:41 +0530

    reducer setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-
 import Notes from './components/Notes/Notes';
 import Login from './components/Login/Login.js';
+import store from './store'
+import { Provider } from 'react-redux'
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
 
-      <Login />
-      {/* <Notes /> */}
-    </div>
+    <Provider store={store}>
+
+
+      <div className="App">
+        <Nav />
+
+        <Login />
+        {/* <Notes /> */}
+      </div>
+
+
+    </Provider>
   );
 }
 
