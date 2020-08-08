3e3dd6d986f86d6bdce65c25642652cba4d7a0c9
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-08-08 14:04:47 +0530

    responsive fixes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Nav from './components/Nav';
 import Notes from './components/Notes/Notes';
 import Login from './components/Login/Login.js';
 import store from './store'
@@ -10,8 +9,7 @@ import { Provider } from 'react-redux'
 import setAuthToken from './utils/setAuthToken'
 import { loadUser } from './actions/auth'
 import PrivateRoute from './components/PrivateRoute';
-
-
+import Nav from './components/Nav.js';
 if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
@@ -26,16 +24,15 @@ function App() {
 
     <Provider store={store}>
       <Router>
-        <div className="App">
+        <div className="App" id="login-div">
           <Nav />
           <Route exact path="/" component={Login} />
           <Switch>
-            {/* TODO: make note route private */}
             <PrivateRoute exact path='/notes' component={Notes} />
           </Switch>
         </div>
       </Router>
-    </Provider>
+    </Provider >
   );
 }
 
