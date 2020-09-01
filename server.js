e783815cf05670c2c5e47a7cc84e09545bdda415
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2020-09-01 20:25:16 +0530

    prepare to host

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000;
+require('dotenv').config()
 
 connectDB();
 
@@ -17,6 +18,16 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/note', require('./routes/api/note'))
 
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+
+  //set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(PORT, () =>
   console.log(`Server listening on port ${PORT}`)
