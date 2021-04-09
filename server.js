7d0ec1574b5285983d03989f941ca8c6e55a01a1
Tarun Soni <soni.tarun26@gmail.com>
2021-04-09 21:57:02 +0530

    prep for dep

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const express = require("express");
-const app = express();
-const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
-require("dotenv").config();
+const express = require('express')
+const app = express()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+const path = require('path')
+require('dotenv').config()
 
-connectDB();
+connectDB()
 
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 //Init middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }))
 
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/note", require("./routes/api/note"));
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/note', require('./routes/api/note'))
 
 // serve static assets in production
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
